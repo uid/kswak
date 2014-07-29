c13a386d4e702867ddce68bd1402138af3b14ec6
stanri <sarivera@mit.edu>
2014-07-29 15:33:17 -0400

    added student support, working on getting the submission data to show in the teacherview

--- kswak_steven.js
+++ kswak_steven.js
@@ -1,11 +1,15 @@
 Questions = new Meteor.Collection("questions");
+//Submissions = new Meteor.Collection("submissions");
 
 if (Meteor.isClient) {
+    Template.home.helpers({
+        questions: function() {
+            return Questions.find();
+        }
+    });
+
     Template.new.events({
         'submit form': function (event, template) {
-//            // template data, if any, is available in 'this'
-//            if (typeof console !== 'undefined')
-//                console.log("Publish button pressed.");
             event.preventDefault();
 
             title = template.find("input[name=title]");
@@ -15,14 +19,14 @@ if (Meteor.isClient) {
             choice4 = template.find("input[name=choice_4]");
             correct = $('input[name="correct"]:checked').val(); //integer, representing the choice that is correct
 
-            //store data
-            var data = {
+            var question_data = {
                 title: title.value,
                 choice1: choice1.value,
                 choice2: choice2.value,
                 choice3: choice3.value,
                 choice4: choice4.value,
-                correct: correct
+                correct: correct,
+                submission_map: {'A': 0, 'B': 0, 'C': 0, 'D': 0},
             }
 
             //reset fields
@@ -35,24 +39,29 @@ if (Meteor.isClient) {
                 this.checked = false;
             });
 
-            Questions.insert(data, function(err) { /* handle error */ });
-//            console.log(data);
+            var question_id = Questions.insert(question_data, function(err) { /* handle error */ });
+            Router.go('/teacher/' + question_id);
+
         }
   });
 
-    Template.teacher_question_view.title = function() {
-        return "test";
-    }
-
-    Template.question_view.title = function() {
-        return "test";
-    }
-
     Template.question_view.events({
-        'click submit': function () {
-            //template data, if any, is available in 'this'
-            if (typeof console !== 'undefined')
-                console.log("You pressed the button!");
+        'submit #student_question': function (event, template) {
+            event.preventDefault();
+            var choice = template.find("input[name='choice']:checked");
+            if (choice == null) {
+                console.log('ERROR: nothing chosen. Please choose an answer.')
+            }
+            else {
+                var user_answer = choice.value;
+                console.log(user_answer);
+                var id = Router.current().path.substr(1);
+                var question = Questions.findOne({_id: id});
+                var submission_map = question.submission_map;
+                submission_map[user_answer] += 1;
+                Questions.update(id, {$set: {submission_map: submission_map}});
+                console.log("submitted!");
+            }
         }
     });
 }
@@ -65,20 +74,21 @@ if (Meteor.isServer) {
 }
 
 //Templates needed: teacher, home, question, teacher_question_view
-//Router.map(function () {
-//    this.route('teacher');  // By default, path = '/teacher', template = 'teacher'
-//    this.route('home', {
-//        path: '/',  //overrides the default '/home'
-//    });
-//    this.route('question_view', {
-//        path: '/:_id',  //overrides the default '/home'
-//        data: function() { return Questions.findOne(this.params._id); },
-//    });
-//    this.route('teacher_question_view', {
-//        path: '/teacher/:_id',
-//    });
-//    this.route('teacher_new', {
-//        path: '/teacher/new',
-//        template: 'new',
-//    });
-//});
+Router.map(function () {
+    this.route('teacher');  // By default, path = '/teacher', template = 'teacher'
+    this.route('home', {
+        path: '/',
+    });
+    this.route('question_view', {
+        path: '/:_id',  //overrides the default '/home'
+        data: function() { return Questions.findOne(this.params._id); },
+    });
+    this.route('teacher_new', {
+        path: '/teacher/new',
+        template: 'new',
+    });
+    this.route('teacher_question_view', {
+        path: '/teacher/:_id',
+        data: function() { return Questions.findOne(this.params._id); },
+    });
+});
