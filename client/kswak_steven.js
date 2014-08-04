c78b41dbbf210b04f1b1c996dfa8e8ae43546557
UID <peitao@mit.edu>
2014-08-04 10:20:04 -0400

    added beginnings of SVG

--- client/kswak_steven.js
+++ client/kswak_steven.js
@@ -175,8 +175,45 @@ if (Meteor.isClient) {
         //     percentC = 100.0*(question.C / total);
         //     percentD = 100.0*(question.D / total);
         // }
-        var a = $('.statistics');
-        console.log(a);
+    }
+
+    Template.teacher_question_view.rendered = function(){
+        // $(this.find("#container_teacher_question_view")).append("<div>GOOD MORNING: "+this.data.title+"</div>");
+        var teachQuestViewTemp = $(this.find("#container_teacher_question_view"));
+        teachQuestViewTemp.append("<br><br>")
+        teachQuestViewTemp.append("<div>Random Temporary Thing: "+ this.data.title+"</div>");
+        teachQuestViewTemp.append("<div class='chart'></div>");
+
+        console.log("THIS THING");
+
+        var percentages = []
+        var optionsLen = this.data.options.length;
+        for (var jj=0; jj < optionsLen; jj++){
+            percentages.push(this.data.options[jj].percent);
+            console.log(this.data.options[jj].percent);
+        }
+        console.log(this);
+
+        var width = 420;
+        var barHeight = 20;
+        var x = d3.scale.linear()
+            .domain([0, d3.max(percentages)])
+            .range([0, width]);
+        var chart = d3.select(".chart")
+            .attr("width", width)
+            .attr("height", barHeight * optionsLen);
+        var bar = chart.selectAll("g")
+            .data(percentages)
+            .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+        bar.append("rect")
+            .attr("width", x)
+            .attr("height", barHeight - 1);
+        bar.append("text")
+            .attr("x", function(d) { return x(d) - 3; })
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .text(function(d) { return d; });
 
     }
 }
