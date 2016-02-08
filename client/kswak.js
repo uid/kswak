382e8dd258fada3c389e9b85dcdd7966b1e12ba8
Rob Miller <rcm@mit.edu>
2016-02-07 19:20:02 -0500

    now using Mousetrap for keyboard shortcuts

--- client/kswak.js
+++ client/kswak.js
@@ -54,24 +54,21 @@ Template.main.events({
         var handled = false;
         var key = event.keyCode;
         var noModifiers = !event.altKey && !event.ctrlKey && !event.metaKey;
-        if ((key >= 50 && key < 58) && noModifiers)  {
-            // number key: 2 through 9
-            var numberOfChoices = key - 48;
-            $(".newQuestion").eq(numberOfChoices-2).click();
-            handled = true;
-        } else if ((key >= 65 && key < 74) && noModifiers) {
-            // letter key: A through G
-            var choice = key - 65;
-            $(".choice").eq(choice).click();
-            handled = true;
-        } else if ((key >= 37 && key <= 40) && noModifiers) {
+        // if ((key >= 50 && key < 58) && noModifiers)  {
+        //     // number key: 2 through 9
+        //     var numberOfChoices = key - 48;
+        //     $(".newQuestion").eq(numberOfChoices-2).click();
+        //     handled = true;
+        // } else 
+        // if ((key >= 65 && key < 74) && noModifiers) {
+        //     // letter key: A through G
+        //     var choice = key - 65;
+        //     $(".choice").eq(choice).click();
+        //     handled = true;
+        // } else 
+        if ((key >= 37 && key <= 40) && noModifiers) {
             // arrow key: move the keyboard focus by simulating Tab or ShiftTab
             var direction = (key == 39 || key == 40) ? 1 : -1;
-            var buttons = $(":button");
-            var nextButtonIndex = buttons.index(event.target) + direction;
-            if (nextButtonIndex < 0) nextButtonIndex = buttons.length-1;
-            else if (nextButtonIndex >= buttons.length) nextButtonIndex = 0;
-            buttons.eq(nextButtonIndex).focus();
         }
 
         if (handled) {
@@ -81,6 +78,39 @@ Template.main.events({
     },
 
 });
+
+// keyboard shortcuts
+Meteor.startup(function () {
+    // number keys 2 through 9 start new quizzes with that number of choices
+    _.range(2,10).map(function(numberOfChoices, index) {
+        Mousetrap.bind(new String(numberOfChoices), function() {
+            $(".newQuestion").eq(index).focus().click();
+        });
+    });
+
+    // letter keys A through G choose that answer button 
+    "ABCDEFG".split("").map(function(letter, index) {
+        function handler() { $(".choice").eq(index).focus().click(); }
+        Mousetrap.bind(letter.toLowerCase(), handler);
+        Mousetrap.bind(letter.toUpperCase(), handler);
+    });
+
+    // arrow keys move the keyboard focus through the buttons
+    function moveKeyboardFocus(direction, event) {
+        var buttons = $(":button");
+        var nextButtonIndex = buttons.index(event.target) + direction;
+        if (nextButtonIndex < 0) nextButtonIndex = buttons.length-1;
+        else if (nextButtonIndex >= buttons.length) nextButtonIndex = 0;
+        buttons.eq(nextButtonIndex).focus();  
+    }
+    var focusBack =    _.partial(moveKeyboardFocus, -1);
+    var focusForward = _.partial(moveKeyboardFocus, +1);
+    Mousetrap.bind("up", focusBack);
+    Mousetrap.bind("left", focusBack);
+    Mousetrap.bind("down", focusForward);
+    Mousetrap.bind("right", focusForward);
+});
+
 
 Template.main.onRendered(function() {
     // give the main body the keyboard focus, to support shortcuts
